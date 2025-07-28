8e98b41c29f0739b57746bd6237b07d54ba679e5
hasanalali2003 <hasanalali887h@gmail.com>
2025-07-28 17:23:31 +0300

    Update request_controller.js, Fix runtime error

--- Backend/controllers/request_controller.js
+++ Backend/controllers/request_controller.js
@@ -127,7 +127,7 @@ const deleteRequest = async (req, res) => {
         const isExists = await Request.exists({ id: requestId });
         if (isExists) {
             // Delete the request from the database.
-            const delete = await Request.findOneAndDelete({ id: requestId });
+            const deleteResult = await Request.findOneAndDelete({ id: requestId });
             return res
                 .status(200)
                 .json({ message: "Request deleted successfully." });
